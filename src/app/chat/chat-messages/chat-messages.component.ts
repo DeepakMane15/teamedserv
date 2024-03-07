583b2fc58a34d94e262c3917175f251d37a73189
vishalgddam873 <vishal18@navgurukul.org>
2024-03-08 01:14:38 +0530

    Chat Messages Component added

---
+++ src/app/chat/chat-messages/chat-messages.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-messages',
+  templateUrl: './chat-messages.component.html',
+  styleUrl: './chat-messages.component.scss'
+})
+export class ChatMessagesComponent {
+  public messages :any =[{
+    "sender":"Vishal Gaddam",
+    "message" : "Hello this is my message",
+    "timing" : "3.00pm",
+    "isUser" : true
+  }, {
+    "sender":"Elon Musk",
+    "message" : "Hello this is my message",
+    "timing" : "3.00pm",
+    "isUser" : false
+  }]
+}
